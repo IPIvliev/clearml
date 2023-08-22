a895ccfc3695a5c6290e9d40ebe8f69f5c00c3ed
IPIvliev <cccp1945@mail.ru>
2023-08-22 17:08:25 +0300

    Without dataset

--- train.py
+++ train.py
@@ -5,18 +5,18 @@ from clearml import Dataset
 # Load the model.
 model = YOLO('yolov8n.pt')
 
-dataset_path = Dataset.get(
-    dataset_name='drone_v2', 
-    dataset_project='Yolov8'
-).get_mutable_local_copy()
+# dataset_path = Dataset.get(
+#     dataset_name='drone_v2', 
+#     dataset_project='Yolov8'
+# ).get_mutable_local_copy()
 
-print(dataset_path)
+# print(dataset_path)
 
 if __name__ == '__main__':
    # Training.
    results = model.train(
-      # data='e:/python/gl_drone_detection/datasets/drone_dataset_v2/data.yaml',
-      data=dataset_path+'/data.yaml',
+      data='e:/python/gl_drone_detection/datasets/drone_dataset_v2/data.yaml',
+    #   data=dataset_path+'/data.yaml',
       imgsz=640,
       epochs=2,
       batch=8,
