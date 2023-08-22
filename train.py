10b97fa6fd15a65a4c44cde2415b4a4bce74cc34
IPIvliev <cccp1945@mail.ru>
2023-08-22 15:09:21 +0300

    first commit

---
+++ train.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+from clearml import Dataset
+
+
+# Load the model.
+model = YOLO('yolov8n.pt')
+
+dataset_path = Dataset.get(
+    dataset_name='drone_v2', 
+    dataset_project='Yolov8'
+).get_local_copy()
+
+
+if __name__ == '__main__':
+   # Training.
+   results = model.train(
+      # data='e:/python/gl_drone_detection/datasets/drone_dataset_v2/data.yaml',
+      data=dataset_path,
+      imgsz=640,
+      epochs=2,
+      batch=8,
+      name='yolov8n_custom_10e',
+      device=0,
+      workers=8)
