f37d91d8df4be047e71526d515c6ab7a9f5b2983
IPIvliev <cccp1945@mail.ru>
2023-08-22 16:56:17 +0300

    Add requirements

--- train.py
+++ train.py
@@ -8,14 +8,15 @@ model = YOLO('yolov8n.pt')
 dataset_path = Dataset.get(
     dataset_name='drone_v2', 
     dataset_project='Yolov8'
-).get_local_copy()
+).get_mutable_local_copy()
 
+print(dataset_path)
 
 if __name__ == '__main__':
    # Training.
    results = model.train(
       # data='e:/python/gl_drone_detection/datasets/drone_dataset_v2/data.yaml',
-      data=dataset_path,
+      data=dataset_path+'/data.yaml',
       imgsz=640,
       epochs=2,
       batch=8,
